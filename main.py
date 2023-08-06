7dddd5a30581c0d551b0eeda1de4a3e3cc5cd932
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-08-06 16:29:43 -0400

    init neural network and loop for learning

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ l1 = layer.Layer(784,392)
 l2 = layer.Layer(392,100)
 l3 = layer.Layer(100,10)
 
+"""
+Loops 3 times over the entire dataset of 60000 pictures.
+Prints the overall accuracy of our network after each iteration
+"""
 for i in range(3):
     number_of_correct = 0
     total_number = 60000
     for img, label in zip(images, labels):
 
+        """
+        Forward propagation
+        """
         l1.forward(img)
         l2.forward(l1.output)
         l3.forward(l2.output)
@@ -27,6 +34,9 @@ for i in range(3):
         if pred_max == target_max:
             number_of_correct += 1
 
+        """
+        Back Propagation to adjust weights
+        """
         l3.backward_start(label)
         l2.backward_next(l3.output, l3.weight_matrix)
         l1.backward_next(l2.output, l2.weight_matrix)
