ae8eeaadd8ec343c2f7f8c7053b6d6a4c4cb7aca
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-08-06 16:26:42 -0400

    label class for testing purposes

--- label.py
+++ label.py
@@ -8,7 +8,7 @@ def get_label(num):
     :return: label matrix of size 1 x 10 where every index is 0 except for the index of the correct number
     """
     if num > 9:
-        raise IndexError("Only identifies single digits between 0 and 9 inclusive")
+        raise IndexError("Only identifies single digits between 0 and 9 inclusive!")
     label_matrix = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     label_matrix[num] = 1
     return label_matrix
