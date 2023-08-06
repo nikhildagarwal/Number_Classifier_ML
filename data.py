57395095ee07d1533ecf28cbae5c344bea73f59d
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-08-06 16:25:27 -0400

    parse image data from .npz file

--- data.py
+++ data.py
@@ -3,8 +3,8 @@ import pathlib
 
 
 def get_mnist():
-    with np.load(f"{pathlib.Path(__file__).parent.absolute()}/data/mnist.npz") as f:
-        images, labels = f["x_train"], f["y_train"]
+    with np.load(f"{pathlib.Path(__file__).parent.absolute()}/data/mnist.npz") as fi:
+        images, labels = fi["x_train"], fi["y_train"]
     images = images.astype("float32") / 255
     images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
     labels = np.eye(10)[labels]
